38808dc81f27718c025171850200b9eb666931c6
DmitryMustk <dmutrymust@gmail.com>
2025-02-04 19:33:31 +0700

    add some test data in main class

--- src/main/java/com/example/demo/WebsiteMonitorApplication.java
+++ src/main/java/com/example/demo/WebsiteMonitorApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import com.example.demo.service.WebsiteMonitoringService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableCaching
 public class WebsiteMonitorApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(WebsiteMonitorApplication.class, args);
+		Map<URI, String> yesterday = Map.of(
+				URI.create("https://site1.com/page1"), "<html>Old Content</html>",
+				URI.create("https://site2.com/about"), "<html>About Us</html>"
+		);
+
+		Map<URI, String> today = Map.of(
+				URI.create("https://site1.com/page1"), "<html>New Content</html>",
+				URI.create("https://site3.com/contact"), "<html>Contact Info</html>"
+		);
+
+		ApplicationContext context = SpringApplication.run(WebsiteMonitorApplication.class, args);
+		WebsiteMonitoringService service = context.getBean(WebsiteMonitoringService.class);
+
+		Optional<String> report = service.buildDailyReport(yesterday, today);
+		report.ifPresent(System.out::println);
 	}
 }
